a35bd338a0518d8996cbf9a97e64c4b9f1b83d42
남민혁 <minhyuk.nam@outlook.com>
2021-03-24 16:02:10 +0000

    reverted main.cpp for submission

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -4,19 +4,10 @@ using namespace std;
 
 int main(void)
 {
-    Str c(5);
-	cout << c.contents()<<endl;
-	cout << c.length()<<endl;
-    c = "I'm a a";
-	cout << c.contents()<<endl;
-	cout << c.length()<<endl;
     Str a("I'm a girl");
-	cout << a.contents()<<endl;
-	cout << a.length()<<endl;
+	cout << a.contents();
     a="I'm a boy\n";
-	cout << a.contents()<<endl;
-	cout << a.length()<<endl;
+	cout << a.contents();
 	cout << a.compare("I'm a a") << endl;
-    cout << a.compare(c) << endl;
 	return 0;
 }
